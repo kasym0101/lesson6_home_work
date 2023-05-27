678829289f71a7606516b570cf992e806a89044c
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-05-27 18:46:53 +0600

    first commit

---
+++ lesson6_home_work.dart
@@ -0,0 +1,149 @@
+void main() {
+// 1
+  // num a = 5;
+  // num b = 5.126;
+  // num c = a + b;
+  // num c1 = a - b;
+  // num c2 = a * b;
+  // num c3 = a / b;
+  // print('${c}, ${c1}, ${c2}, ${c3}');
+
+//2
+  // int a2 = 5;
+  // int b2 = 2;
+  // print(a2 + b2);
+
+  // int a3 = 5;
+  // int b3 = 2;
+  // print(a3 - b3);
+
+  // int a4 = 132;
+  // int b4 = 123;
+  // print(a4 * b4);
+
+  // num a5 = 78;
+  // num b5 = 2;
+  // num c5 = 2.5;
+  // print(a5 / b5);
+  // print(a5 / c5);
+
+  // num a6 = 0;
+  // num b6 = 5;
+  // print(a6 * b6);
+
+//3
+  // //   int a12 = 1;
+  // // int b12 = 2;
+  // // a12 = a12 + b12;
+  // // b12 = a12 - b12;
+  // // a12 = a12 - b12;
+  // // print('a12 = $a12');
+  // // print('b12 = $b12');
+
+//4
+  // // print('Введите Ваше имя:');
+  // // String name = stdin.readLineSync()!;
+  // // print('Введите Ваш возраст:');
+  // // int age = int.parse(stdin.readLineSync()!);
+  // // print('Вас зовут: $name, Ваш возраст: $age');
+
+//5
+  // // print('Введите номер телефона:');
+  // // String phoneNumber = stdin.readLineSync()!;
+  // // if (phoneNumber.length == 13) {
+  // //   print('Номер $phoneNumber введен верно.');
+  // // } else {
+  // //   print('Номер $phoneNumber введен неверно. Повторите попытку.');
+  // // }
+
+//6
+  // // bool isRain = true;
+  // // isRain == true ? print('Возьмите зонт') : print('Зонт не нужен');
+
+//7
+  // num a7 = 3;
+  // a7 >= 3 ? print(a7 + 10) : print(a7 - 10);
+
+//8
+  // num a8 = 10;
+  // if (a8 <= 7) {
+  //   print('YES');
+  // } else if (a8 >= 10) {
+  //   print('NO');
+  // } else {
+  //   print('ERROR');
+  // }
+
+//9
+  // num a9 = 4;
+  // num b9 = 6;
+  // if (a9 > b9) {
+  //   print('$a9');
+  // } else if (a9 < b9) {
+  //   print('$b9');
+  // } else {
+  //   print('Числа равны');
+  // }
+
+//10
+  // num a10 = 90;
+  // num b10 = 10;
+  // a10 + b10 == 100 ? print('Yes') : print('No');
+
+//11
+  // print('Введите номер месяца:');
+  // int month = int.parse(stdin.readLineSync()!);
+  // if (month == 12 || month == 1 || month == 2) {
+  //   print('Зима');
+  // } else if (month == 3 || month == 4 || month == 5) {
+  //   print('Весна');
+  // } else if (month == 6 || month == 7 || month == 8) {
+  //   print('Лето');
+  // } else if (month == 9 || month == 10 || month == 11) {
+  //   print('Осень');
+  // } else {
+  //   print('Введите номер месяца от 1 до 12');
+  // }
+
+//12
+//   num a11 = 7;
+//   num b11 = 6;
+//   if (a11 > b11) {
+//     print('$b11');
+//   } else if (a11 < b11) {
+//     print('$a11');
+//   } else {
+//     print('Числа равны');
+//   }
+
+//13
+  // int a13 = 321;
+  // print(a13.toString().split('').reversed.join());
+
+//14
+  // List<int> a12 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  // for (int i = 0; i < a12.length; i++) {
+  //   if (a12[i] < 5) {
+  //     print(a12[i]);
+  //   }
+  // }
+
+//15
+  // int n = 5;
+  // String stars = '';
+
+  // for (int i = 1; i <= n; i++) {
+  //   stars += '*';
+  //   print(stars);
+  // }
+
+//16
+  // for (int i = 1; i <= 5; i++) {
+  //   print(i);
+  // }
+
+//17
+  // for (int i = 5; i >= 1; i--) {
+  //   print(i);
+  // }
+}
